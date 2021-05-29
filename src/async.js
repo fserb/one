9562f1e5263e86a71ea8bf514d3ab21aedea81eb
Fernando Serboncini <fserb@fserb.com.br>
2021-05-29 02:40:16 -0400

    full belt

--- src/async.js
+++ src/async.js
@@ -1,19 +1,6 @@
 /*
 ASYNC Arcade
 
-- request belt
-
-- requests:
-  - number of individual blocks, generic or color
-  - certain shape, generic or color
-  - blocks per second
-  - sync creation
-  - request OR request
-
-- milestones:
-  - add color 3, 4
-  - add speed
-
 */
 
 import * as one from "./one/one.js";
@@ -48,12 +35,22 @@ const SELECTED = [null, null];
 const GRAVITY = {x: 1, y: 0};
 let NEEDS_MERGE = false;
 const BELT = [];
-let BELT_SPEED = 1;
-let COLORS = 2;
+let BELT_POS;
+let BELT_SPEED;
+let COLORS;
+let BELT_DONE;
+let BELT_NEXT;
+const MERGE_COUNT = [0, 0];
+let BELT_MILESTONE = 0;
 
 function init() {
   BELT.length = 0;
   BOARD.length = 0;
+  COLORS = 2;
+  BELT_POS = 1;
+  BELT_SPEED = 1;
+  BELT_DONE = 0;
+  BELT_NEXT = 1;
   SELECTED[0] = SELECTED[1] = null;
 
   fillEmpty();
@@ -71,6 +68,13 @@ function createBlock(p) {
   }, p);
   BOARD.push(o);
   return o;
+}
+
+function toScreen(p) {
+  const bp = BOARDPOS[p.b];
+  return {
+    x: p.d.x + bp.x + (p.w / 2 + p.x) * SIZE,
+    y: p.d.y + bp.y + (p.h / 2 + p.y) * SIZE };
 }
 
 function fillEmpty(step = 0) {
@@ -229,6 +233,7 @@ function tryMergeBlocks() {
     }
     b.w += exp.x;
     b.h += exp.y;
+    MERGE_COUNT[b.b]++;
 
     return tryMergeBlocks();
   }
@@ -271,6 +276,7 @@ async function updateClick() {
       .then(() => {
         BOARD.remove(ONE);
       });
+    actBelt(ONE);
     await Promise.sleep(0.1);
     fallBlocks();
     SELECTED[0] = SELECTED[1] = null;
@@ -290,7 +296,6 @@ async function updateClick() {
 
       s0.d = vec.sub(p1, p0);
       s1.d = vec.sub(p0, p1);
-
 
       const T = 0.35;
       act(s0.d)
@@ -321,7 +326,7 @@ async function updateClick() {
   }
 }
 
-function update(dt) {
+function updatePieces(dt) {
   for (const p of BOARD) {
     if (!p.selected) {
       if (p.scale < 1) {
@@ -333,13 +338,215 @@ function update(dt) {
       }
     }
   }
+}
 
-  updateClick();
+function actBeltMerge() {
+  if (BELT.length == 0) return;
+  if (MERGE_COUNT[0] == 0 || MERGE_COUNT[1] == 0) return;
+
+  const b = BELT[BELT.length - 1];
+
+  let vs = [];
+  if (b.type == "sync") vs.push(b);
+  if (b.type == "or" && b.a.type == "sync") vs.push(b.a);
+  if (b.type == "or" && b.b.type == "sync") vs.push(b.b);
+
+  if (vs.length == 0) return;
+
+  for (const v of vs) {
+    act(v)
+      .attr("t", 0, 0.3, ease.linear)
+      .then(() => popBelt(v));
+  }
+}
+
+function popBelt(b) {
+  if (b.parent === undefined) {
+    BELT_DONE++;
+    BELT.pop();
+  } else {
+    act(b.parent)
+      .attr("t", 0, 0.3, ease.linear)
+      .then(() => popBelt(b.parent));
+    }
+}
+
+function actBelt(piece, b = null) {
+  if (BELT.length == 0) return;
+
+  const area = piece.w * piece.h;
+  b = b ?? BELT[BELT.length - 1];
+
+  if (b.type == "blocks") {
+    if (b.color == -1 || b.color == piece.v) {
+      act(b)
+        .then(() => { b.t = 1; b.count = area; })
+        .attr("t", 0, 0.3, ease.linear)
+        .then(() => {
+          b.total -= area;
+          b.count = 0;
+          if (b.count >= b.total) {
+            popBelt(b);
+          }
+        });
+    }
+    return;
+  }
+
+  if (b.type == "shape") {
+    if (b.color == -1 || b.color == piece.v) {
+      if (b.width == piece.w && b.height == piece.h) {
+        act(b)
+          .attr("t", 0, 0.3, ease.linear)
+          .then(() => popBelt(b));
+        }
+    }
+    return;
+  }
+
+  if (b.type == "bps") {
+    if (b.color == -1 || b.color == piece.v) {
+      b.value = Math.min(b.max, b.value + area);
+      if (b.value >= b.max) {
+        act(b)
+          .attr("t", 0, 0.3, ease.linear)
+          .then(() => popBelt(b));
+        }
+    }
+  }
+
+  if (b.type == "or") {
+    actBelt(piece, b.a);
+    actBelt(piece, b.b);
+  }
+}
+
+function makeBeltRequest(canBeOr = true) {
+  const maxtype = canBeOr ? 4 : 3;
+  const type = Math.floor(maxtype * Math.random());
+
+  if (type == 0) {
+    return {
+      type: "blocks",
+      color: Math.random() < 0.5 ? -1 : Math.floor(COLORS * Math.random()),
+      total: Math.floor(4 + 12 * Math.random()),
+      count: 0,
+      t: 0.0,
+    };
+  }
+
+  if (type == 1) {
+    let w = Math.random() < 0.75 ? 2 : Math.random() < 0.75 ? 3 : 4;
+    let h = Math.random() < 0.75 ? 2 : Math.random() < 0.75 ? 3 : 4;
+    if (w * h > 12) {
+      if (w == 4) w = 3;
+      if (h == 4) h = 3;
+    }
+
+    return {
+      type: "shape",
+      color: Math.random() < 0.5 ? -1 : Math.floor(COLORS * Math.random()),
+      width: w,
+      height: h,
+      t: 1.0,
+    };
+  }
+
+  if (type == 2) {
+    return {
+      type: "bps",
+      color: Math.random() < 0.5 ? -1 : Math.floor(COLORS * Math.random()),
+      max: 10 + BELT_SPEED * 0.1,
+      value: 0,
+      speed: 0.1 + 0.4 * Math.random(),
+      t: 1.0,
+    };
+  }
+
+  if (type == 3) {
+    return {
+      type: "sync",
+      t: 1.0,
+    }
+  }
+
+  if (type == 4) {
+    const a = makeBeltRequest(false);
+    const b = makeBeltRequest(false);
+    const x = {
+      type: "or",
+      t: 1.0,
+      a: a,
+      b: b,
+    }
+    a.parent = b.parent = x;
+    return x;
+  }
+
+  return null;
+}
+
+function makeBeltMilestone() {
+  const act = BELT_MILESTONE++;
+
+  const o = {type: "milestone", color: -1, t: 1};
+
+  if (act == 0) return Object.assign(o, { action: () => BELT_SPEED++ });
+  if (act == 1) return Object.assign(o, { color: 2, action: () => COLORS++ });
+  if (act == 2) return Object.assign(o, { action: () => BELT_SPEED++ });
+  if (act == 3) return Object.assign(o, { color: 3, action: () => COLORS++ });
+  return Object.assign(o, { action: () => BELT_SPEED++ });
+}
+
+function updateBelt(dt) {
+  BELT_POS += (BELT.length <= 1 || (BELT.length == 2 && BELT_POS < 0.1) ?
+    dt * BELT_SPEED : (dt * BELT_SPEED / 100));
+
+  if (BELT_POS >= 1) {
+    BELT_POS -= 1;
+    if (BELT_NEXT <= 0) {
+      BELT.unshift(makeBeltMilestone());
+      BELT_NEXT += 5;
+    } else {
+      BELT.unshift(makeBeltRequest());
+      BELT_NEXT--;
+    }
+  }
+
+  if (BELT.length == 0) return;
 
   if (act.is()) return;
 
+  const b = BELT[BELT.length - 1];
+
+  if (b.type == "milestone") {
+    b.action();
+    act(b)
+      .attr("t", 0, 0.3, ease.quadIn)
+      .then(() => popBelt(b));
+    return;
+  }
+
+  let vs = [];
+  if (b.type == "bps") vs.push(b);
+  if (b.type == "or" && b.a.type == "bps") vs.push(b.a);
+  if (b.type == "or" && b.b.type == "bps") vs.push(b.b);
+
+  for (const v of vs) {
+    v.value = Math.max(0, v.value - dt * v.speed);
+  }
+}
+
+function update(dt) {
+  updatePieces(dt);
+  updateBelt(dt);
+  updateClick();
+
+  if (act.is()) return;
   if (NEEDS_MERGE) {
+    MERGE_COUNT[0] = MERGE_COUNT[1] = 0;
     tryMergeBlocks();
+    actBeltMerge();
     NEEDS_MERGE = false;
   }
 }
@@ -362,7 +569,130 @@ function render(ctx) {
   for (const p of post) {
     renderPiece(ctx, p);
   }
+
+  renderBelt(ctx);
 }
+
+const SZ = 100;
+function renderBelt(ctx) {
+  ctx.save();
+  ctx.fillStyle = "red";
+  ctx.translate(0, (290 + 44 - SZ) / 2);
+  ctx.scale(SZ, SZ);
+
+
+  let p = (BELT_POS - 1) * 1.2;
+  for (const b of BELT) {
+    if (b === null) continue;
+    ctx.save();
+    ctx.translate(p, 0);
+    renderBeltUnit(ctx, b);
+    ctx.restore();
+    p+= 1.2;
+  }
+
+  ctx.restore();
+}
+
+function renderBeltUnit(ctx, b) {
+  if (b.type == "blocks") renderBeltBlocks(ctx, b);
+  else if (b.type == "shape") renderBeltShape(ctx, b);
+  else if (b.type == "bps") renderBeltBPS(ctx, b);
+  else if (b.type == "sync") renderBeltSync(ctx, b);
+  else if (b.type == "or") renderBeltOR(ctx, b);
+  else if (b.type == "milestone") renderMilestone(ctx, b);
+}
+
+function renderMilestone(ctx, b) {
+  ctx.globalAlpha = b.t;
+  ctx.fillStyle = b.color == -1 ? C.fg : C.piece[b.color];
+
+  ctx.beginPath();
+  for (let i = 0; i < 10; ++i) {
+    const r = (i % 2 == 0) ? 0.35 : 0.15;
+    const f = i / 10 - 0.05;
+    ctx.lineTo(0.5 + r * Math.cos(Math.TAU * f),
+    0.5 + r * Math.sin(Math.TAU * f));
+  }
+  ctx.fill();
+}
+
+function renderBeltOR(ctx, b) {
+  ctx.globalAlpha = b.t;
+
+  ctx.save();
+  ctx.translate(0, -0.5);
+  ctx.scale(0.9, 0.9);
+  ctx.translate(0.05, 0.05);
+  // ctx.fillStyle = "#DDD";
+  // ctx.fillRect(0, 0, 1, 1);
+  renderBeltUnit(ctx, b.a);
+  ctx.restore();
+
+  ctx.save();
+  ctx.translate(0, 0.5);
+  ctx.scale(0.9, 0.9);
+  ctx.translate(0.05, 0.05);
+  // ctx.fillStyle = "#DDD";
+  // ctx.fillRect(0, 0, 1, 1);
+  renderBeltUnit(ctx, b.b);
+  ctx.restore();
+}
+
+function renderBeltSync(ctx, b) {
+  ctx.globalAlpha *= b.t;
+  ctx.fillStyle = C.fg;
+
+  ctx.fillRect(11.21/51, 14.68/51, 13/51, 17/51);
+  ctx.fillRect(26.79/51, 19.93/51, 13/51, 17/51);
+  ctx.fillRect(23.32/51, 14.68/51, 9.97/51, 3/51);
+  ctx.fillRect(17.71/51, 33.93/51, 9.97/51, 3/51);
+}
+
+function renderBeltBPS(ctx, b) {
+  ctx.globalAlpha *= b.t;
+  ctx.fillStyle = ctx.strokeStyle = b.color == -1 ? C.fg : C.piece[b.color];
+
+  const v = b.value / b.max;
+
+  ctx.fillRect(0.39, 0.96 - v * 0.92, 0.22, v * 0.92);
+
+
+  ctx.lineWidth = 0.02;
+  ctx.strokeRect(0.35, 0, 0.3, 1);
+}
+
+function renderBeltShape(ctx, b) {
+  ctx.globalAlpha *= b.t;
+
+  const dx = (1 - b.width * 0.25) / 2;
+  const dy = (1 - b.height * 0.25) / 2;
+
+  ctx.fillStyle = ctx.strokeStyle = b.color == -1 ? C.fg : C.piece[b.color];
+  for (let x = 0; x < b.width; ++x) {
+    for (let y = 0; y < b.height; ++y) {
+      ctx.fillRect(dx + x * 0.25 + 0.025, dy + y * 0.25 + 0.025, 0.20, 0.20);
+    }
+  }
+}
+
+function renderBeltBlocks(ctx, b) {
+  const base = ctx.globalAlpha;
+  ctx.fillStyle = ctx.strokeStyle = b.color == -1 ? C.fg : C.piece[b.color];
+
+  const dx = (1 - 0.25 * Math.ceil(b.total / 4)) / 2;
+
+  for (let i = 0; i < b.total; ++i) {
+    const y = i % 4;
+    const x = (i - y) / 4;
+
+    ctx.globalAlpha = base * ((i >= b.total - b.count) ? b.t : 1);
+    ctx.lineWidth = 0.02;
+    ctx.strokeRect(dx + x * 0.25 + 0.025, y * 0.25 + 0.025, 0.20, 0.20);
+    ctx.fillRect(dx + x * 0.25 + 0.07 + 0.025, y * 0.25 + 0.07 + 0.025, 0.06, 0.06);
+  }
+}
+
 
 const B = 0.04347826 * 1.5;
 const S = 0.04347826 * 1.3;
@@ -395,13 +725,6 @@ function renderPiece(ctx, p) {
   ctx.fillRect(B , B, p.w - B * 2, p.h - B * 2);
 
   ctx.restore();
-}
-
-function toScreen(p) {
-  const bp = BOARDPOS[p.b];
-  return {
-    x: p.d.x + bp.x + (p.w / 2 + p.x) * SIZE,
-    y: p.d.y + bp.y + (p.h / 2 + p.y) * SIZE };
 }
 
 export default one.game(init, update, render);
